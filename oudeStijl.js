92447bfc61d936d7ae3906367842573a218f5119
SimonsPCC <73341402+SimonsPCC@users.noreply.github.com>
2021-01-27 01:19:57 +0100

    Add files via upload

---
+++ oudeStijl.js
@@ -0,0 +1,18 @@
+let colors = ["yellow", "blue", 'red', 'orange'];
+let i = 0;
+//while loop
+while (i < colors.length) {
+    console.log(colors[i]);
+    i++;
+}
+//for loop
+
+for (let i = 0; i < colors.length; i++) {
+    console.log(colors[i])
+
+}
+//forEach
+colors.forEach((val) => {
+    console.log(val)
+});
+
